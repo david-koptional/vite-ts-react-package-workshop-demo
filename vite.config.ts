7efc08fb46ca18238fa6755955efd23f3104f244
david@koptional <david@koptional.com>
2024-02-27 08:33:16 -0700

    feat: configure vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/index.ts", // Your entry here
+      name: "MyReactPage", // Name of your package here
+      fileName: (format) => `my-package.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
